3790dc631ac1a81dcdc9c2a53513f8647e91c599
refda <s@merq.de>
2017-12-08 15:36:18 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
     if((navigator.connection.type).toUpperCase() != "NONE" &&
        (navigator.network.connection.type).toUpperCase() != "UNKNOWN") {
     //ONLINE
-    window.location.href="http://www.briefklick.de/app/?ios=1";       
+    window.location.href="https://web.whatsapp.com/";       
     } else {
     window.location.href="offline.html";
     }        
